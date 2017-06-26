d10daea3550b38cc877dcf4bba912bc98e94d89e
Andrew <bloodfurry9595@gmail.com>
2017-06-26 15:00:37 +0300

    +ajax call for json content

--- js/script.js
+++ js/script.js
@@ -1,206 +1,21 @@
 // uploading images
+//ajax call for .json file at github
+var imgJSON = $.ajax({
+	url: 'https://rawgit.com/AndrewRicfo/photoGallery/master/js/jason.json',
+	data: {},
+	type: 'GET',
+	crossDomain: true,
+	dataType: 'json',
+	context: document.body,
+	global: false,
+	async:false,
+	success: function(result) {
+		return result;
+	},
+	error: function() { alert('Failed to get .json content!'); },
+}).responseText;
 
-var imgJSON = [
-{
-	"srcThumb": "../fproject/images/thumbnails/th-1.jpg",
-	"srcFull": "../fproject/images/full/1.jpg",
-	"alt": "1 photo description",
-	"author": "Author 1",
-	"likes": 1,
-	"width": 1024,
-	"height": 1280 
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-2.jpg",
-	"srcFull": "../fproject/images/full/2.jpg",
-	"alt": "2 photo description",
-	"author": "Author 1",
-	"likes": 2,
-	"width": 1280,
-	"height": 1170
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-3.jpg",
-	"srcFull": "../fproject/images/full/3.jpg",
-	"alt": "3 photo description",
-	"author": "Author 2",
-	"likes": 3,
-	"width": 1280,
-	"height": 853
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-4.jpg",
-	"srcFull": "../fproject/images/full/4.jpg",
-	"alt": "4 photo description",
-	"author": "Author 4",
-	"likes": 4,
-	"width": 1280,
-	"height": 788
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-5.jpg",
-	"srcFull": "../fproject/images/full/5.jpg",
-	"alt": "5 photo description",
-	"author": "Author 5",
-	"likes": 5,
-	"width": 1280,
-	"height": 720
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-6.jpg",
-	"srcFull": "../fproject/images/full/6.jpg",
-	"alt": "6 photo description",
-	"author": "Author 6",
-	"likes": 6,
-	"width": 1280,
-	"height": 899
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-7.jpg",
-	"srcFull": "../fproject/images/full/7.jpg",
-	"alt": "7 photo description",
-	"author": "Author 7",
-	"likes": 7,
-	"width": 1280,
-	"height": 853
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-8.jpg",
-	"srcFull": "../fproject/images/full/8.jpg",
-	"alt": "8 photo description",
-	"author": "Author 8",
-	"likes": 8,
-	"width": 854,
-	"height": 1280
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-9.jpg",
-	"srcFull": "../fproject/images/full/9.jpg",
-	"alt": "9 photo description",
-	"author": "Author 9",
-	"likes": 9,
-	"width": 1280,
-	"height": 853
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-10.jpg",
-	"srcFull": "../fproject/images/full/10.jpg",
-	"alt": "10 photo description",
-	"author": "Author 10",
-	"likes": 10,
-	"width": 864,
-	"height": 1280
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-11.jpg",
-	"srcFull": "../fproject/images/full/11.jpg",
-	"alt": "1 photo description",
-	"author": "Author 11",
-	"likes": 11,
-	"width": 1280,
-	"height": 853 
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-12.jpg",
-	"srcFull": "../fproject/images/full/12.jpg",
-	"alt": "12 photo description",
-	"author": "Author 12",
-	"likes": 12,
-	"width": 1280,
-	"height": 854
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-13.jpg",
-	"srcFull": "../fproject/images/full/13.jpg",
-	"alt": "13 photo description",
-	"author": "Author 13",
-	"likes": 13,
-	"width": 1280,
-	"height": 853
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-14.jpg",
-	"srcFull": "../fproject/images/full/14.jpg",
-	"alt": "14 photo description",
-	"author": "Author 14",
-	"likes": 14,
-	"width": 1280,
-	"height": 854
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-15.jpg",
-	"srcFull": "../fproject/images/full/15.jpg",
-	"alt": "15 photo description",
-	"author": "Author 15",
-	"likes": 15,
-	"width": 853,
-	"height": 1280
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-16.jpg",
-	"srcFull": "../fproject/images/full/16.jpg",
-	"alt": "16 photo description",
-	"author": "Author 16",
-	"likes": 16,
-	"width": 853,
-	"height": 1280
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-17.jpg",
-	"srcFull": "../fproject/images/full/17.jpg",
-	"alt": "17 photo description",
-	"author": "Author 17",
-	"likes": 17,
-	"width": 1280,
-	"height": 853
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-18.jpg",
-	"srcFull": "../fproject/images/full/18.jpg",
-	"alt": "18 photo description",
-	"author": "Author 18",
-	"likes": 18,
-	"width": 1280,
-	"height": 853
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-19.jpg",
-	"srcFull": "../fproject/images/full/19.jpg",
-	"alt": "19 photo description",
-	"author": "Author 19",
-	"likes": 19,
-	"width": 854,
-	"height": 1280
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-20.jpg",
-	"srcFull": "../fproject/images/full/20.jpg",
-	"alt": "20 photo description",
-	"author": "Author 20",
-	"likes": 20,
-	"width": 900,
-	"height": 980
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-21.jpg",
-	"srcFull": "../fproject/images/full/21.jpg",
-	"alt": "21 photo description",
-	"author": "Author 21",
-	"likes": 21,
-	"width": 1280,
-	"height": 1024
-},
-{
-	"srcThumb": "../fproject/images/thumbnails/th-22.jpg",
-	"srcFull": "../fproject/images/full/22.jpg",
-	"alt": "22 photo description",
-	"author": "Author 22",
-	"likes": 22,
-	"width": 750,
-	"height": 1110
-} 
-]
-
+imgJSON = $.parseJSON(imgJSON)
 
 //collect all info from json
 var allImages = ""
@@ -233,8 +48,10 @@ y = w.innerHeight;
 $(document).scroll(function() {
 
 	if($(window).scrollTop() > y-50){
-
-		$('.nav').css({"background-color": "rgba(0,0,0,0.6", "padding-top" : "2px", "height":"40px"})
+		if(x>1200)
+			$('.nav').css({"background-color": "rgba(0,0,0,0.6", "padding-top" : "2px", "height":"55px"})
+		else
+			$('.nav').css({"background-color": "rgba(0,0,0,0.6", "padding-top" : "2px", "height":"40px"})
 		$('.nav-divide').css("margin-top", "10px")
 
 
@@ -413,12 +230,16 @@ for(var i=0; i<n; i++){
 	img.onclick = function(){
 		if(ratio>1){
 			if(modalW==x*0.85)
-				modalImg.style.margin = "10% auto"
+				modalImg.style.margin = "20% auto"
 			else
 				modalImg.style.margin="0 auto"
 		}
-		else
-			modalImg.style.margin="50% auto"
+		else{
+			if(window.innerWidth < 1000)
+				modalImg.style.margin="50% auto"
+			else
+				modalImg.style.margin="5% auto"
+		}
 		modal.style.display = "block";
 		modalImg.style.width = modalW+"px"
 		modalImg.style.height = modalH+"px"
@@ -489,12 +310,16 @@ for(var i=0; i<n; i++){
 		modalImg.style.height = modalH + "px"
 		if(ratio>1){
 			if(modalW==x*0.85)
-				modalImg.style.margin = "10% auto"
+				modalImg.style.margin = "20% auto"
 			else
 				modalImg.style.margin="0 auto"
 		}
-		else
-			modalImg.style.margin="50% auto 2%"
+		else{
+			if(window.innerWidth < 1000)
+				modalImg.style.margin="50% auto"
+			else
+				modalImg.style.margin="5% auto"
+		}
 		modalImg.src = newSrc
 
 	}
@@ -534,12 +359,16 @@ for(var i=0; i<n; i++){
 		modalImg.style.height = modalH + "px"
 		if(ratio>1){
 			if(modalW==x*0.85)
-				modalImg.style.margin = "10% auto"
+				modalImg.style.margin = "20% auto"
 			else
 				modalImg.style.margin="0 auto"
 		}
-		else
-			modalImg.style.margin="50% auto 2%"
+		else{
+			if(window.innerWidth < 1000)
+				modalImg.style.margin="50% auto"
+			else
+				modalImg.style.margin="5% auto"
+		}
 		modalImg.src = newSrc	
 		
 	}	
@@ -571,10 +400,7 @@ window.onload = function() {
 };
 
 
-
-
-//pri klike stal propadat' overlay 
-
+//likes
 $(document).on('click', '.fa-heart-o', function(event){
 	event.preventDefault();
 
@@ -592,30 +418,7 @@ $(document).on('click', '.fa-heart', function(event){
 });
 
 
-
-
-//ajax call for .json file at github
-/*
-var imgJSON = $.ajax({
-	url: 'https://rawgit.com/AndrewRicfo/photoGallery/master/js/jason.json',
-	data: {},
-	type: 'GET',
-	crossDomain: true,
-	dataType: 'json',
-	context: document.body,
-	global: false,
-	async:false,
-	success: function(result) {
-		return result;
-	},
-	error: function() { alert('Failed to get .json content!'); },
-}).responseText;
-
-imgJSON = $.parseJSON(imgJSON)
-console.log(imgJSON[0])
-*/
-
-
+//map
 function loadMap() {
 
 	var latlng = new google.maps.LatLng(50.449631, 30.449989);
@@ -635,3 +438,24 @@ function loadMap() {
 	});
 
 }
+
+
+
+
+
+// var imgJSON = $.ajax({
+// 	url: 'https://rawgit.com/AndrewRicfo/photoGallery/master/js/jason.json',
+// 	data: {},
+// 	type: 'GET',
+// 	crossDomain: true,
+// 	dataType: 'json',
+// 	context: document.body,
+// 	global: false,
+// 	async:false,
+// 	success: function(result) {
+// 		return result;
+// 	},
+// 	error: function() { alert('Failed to get .json content!'); },
+// }).responseText;
+
+// imgJSON = $.parseJSON(imgJSON)
